185dafc00d9e2fad1031f263ef6ee3380468eda8
Erfei Yu <105307186+454270186@users.noreply.github.com>
2024-03-22 14:17:37 +0800

    to: tailwindcss set-up

---
+++ src/pages/login.jsx
@@ -0,0 +1,11 @@
+import { Helmet } from 'react-helmet-async';
+
+export default function Login() {
+    return (
+        <>
+            <Helmet>
+                <title>Login</title>
+            </Helmet>
+        </>
+    )
+}
