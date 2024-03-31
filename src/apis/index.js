df7e323b716091f01d798452ebe968208d8a3c44
王苾留 <12028510+wang-biliu@user.noreply.gitee.com>
2024-03-31 21:26:27 +0800

    feat: display user profile photo

--- src/apis/index.js
+++ src/apis/index.js
@@ -7,7 +7,7 @@ export const HOST_API = 'http://43.136.232.116:5000/test';
 export const apiInstance = axios.create({
   baseURL: HOST_API,
   headers: {
-    Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTE4Nzg1NTQsImlzQWRtaW4iOnRydWUsInVzZXJJRCI6IjEyMzEyMzEyMyJ9.-az1mJ_3tSVtK16uD5Sg7kDU9Gf-GQd4Pq19i2UbYOk'}`, 
+    Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTE5NzcxMjQsImlzQWRtaW4iOnRydWUsInVzZXJJRCI6IjEyMzEyMzEyMyJ9.HDWb49tJJhs5qlE1c_k9WUUoNEIqtONk_D1fR3vvg0M'}`, 
   }
 });
 
